8697e4db750a7372f1bc9e0ce4ae58a423cefc92
Cerdic <cedric@yterium.com>
2019-06-07 16:35:04 +0200

    don't agregate subtests in a same test name because it makes false failure on additional newlines between each one

--- tests/ScssTest.php
+++ tests/ScssTest.php
@@ -130,6 +130,12 @@ class ScssTest extends \PHPUnit_Framework_TestCase
                     ) {
                         $state = 2; // get css
                         $style = isset($matches[1]) ? $matches[1] : null;
+                        // another subtest in the same def name, insert each separately to avoid border errors on newlines between each subtest
+                        if (count($css) && count($scss)) {
+                            $tests[] = array($name, implode($scss), implode($css), $style);
+                            $scss = array();
+                            $css = array();
+                        }
                         continue 2;
                     }
 
