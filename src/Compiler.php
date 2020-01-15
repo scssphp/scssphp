scssphp/scssphp

commit 2bea5ef6b06e2ae527f102808d135d654d1b4391

Improve the @extend compliance with sass-spec : the result order of class extending class is a bit modified because of some logic in combineSelectorSingle() in order to keep class an id before pseudo or []

src/Compiler.php
@@ -863,18 +863,19 @@ class Compiler
         $out    = [];
         $wasTag = false;
 
-        foreach ([$base, $other] as $single) {
+        foreach ([array_reverse($base), array_reverse($other)] as $single) {
             foreach ($single as $part) {
-                if (preg_match('/^[\[.:#]/', $part)) {
+                if (preg_match('/^[\[:]/', $part)) {
                     $out[] = $part;
+                    $wasTag = false;
+                } elseif (preg_match('/^[\.#]/', $part)) {
+                    array_unshift($out, $part);
                     $wasTag = false;
                 } elseif (preg_match('/^[^_-]/', $part)) {
                     $tag[] = $part;
                     $wasTag = true;
                 } elseif ($wasTag) {
                     $tag[count($tag) - 1] .= $part;
-                } elseif (count($out)) {
-                    $out[count($out) - 1] .= $part;
                 } else {
                     $out[] = $part;
                 }

tests/outputs/extending_compound_selector.css
@@ -1,10 +1,10 @@
 .foo:after, .bar:after, .bar:before, .baz.bar:before {
   color: red; }
-.foo:hover:after, .bar:hover:after, .bar:before:hover, .baz.bar:before:hover {
+.foo:hover:after, .bar:after:hover, .bar:before:hover, .baz.bar:before:hover {
   color: blue; }
 
 a, a:link, a:visited, a:active, a:hover {
   color: #fff; }
 
-.nest a.some-class, .nest a:link.some-class, .nest a:visited.some-class, .nest a:active.some-class, .nest a:hover.some-class {
+.nest a.some-class, .nest a.some-class:link, .nest a.some-class:visited, .nest a.some-class:active, .nest a.some-class:hover {
   color: #ccc; }

tests/outputs_numbered/extending_compound_selector.css
@@ -4,7 +4,7 @@
   color: red; }
 
 /* line 5, inputs/extending_compound_selector.scss */
-.foo:hover:after, .bar:hover:after, .bar:before:hover, .baz.bar:before:hover {
+.foo:hover:after, .bar:after:hover, .bar:before:hover, .baz.bar:before:hover {
   color: blue; }
 
 /* line 10, inputs/extending_compound_selector.scss */
@@ -19,5 +19,5 @@ a, a:link, a:visited, a:active, a:hover {
 /* line 33, inputs/extending_compound_selector.scss */
 /* line 37, inputs/extending_compound_selector.scss */
 /* line 41, inputs/extending_compound_selector.scss */
-.nest a.some-class, .nest a:link.some-class, .nest a:visited.some-class, .nest a:active.some-class, .nest a:hover.some-class {
+.nest a.some-class, .nest a.some-class:link, .nest a.some-class:visited, .nest a.some-class:active, .nest a.some-class:hover {
   color: #ccc; }

tests/outputs/extends.css
@@ -19,7 +19,7 @@ hello {
 a:hover, .hoverlink, #demo .overview .fakelink:hover {
   text-decoration: underline; }
 
-div.hello.world.hmm, pre div.okay.span.world.hmm, pre #butt .umm div.sure.span.world.hmm, #butt .umm pre div.sure.span.world.hmm, code div.okay.span.world.hmm, code #butt .umm div.sure.span.world.hmm, #butt .umm code div.sure.span.world.hmm {
+div.hello.world.hmm, pre div.world.hmm.okay.span, pre #butt .umm div.world.hmm.span.sure, #butt .umm pre div.world.hmm.span.sure, code div.world.hmm.okay.span, code #butt .umm div.world.hmm.span.sure, #butt .umm code div.world.hmm.span.sure {
   color: blue; }
 
 .xxxxx .xxxxx .xxxxx, code .xxxxx .xxxxx, code code .xxxxx, code code code, code .xxxxx code, .xxxxx code .xxxxx, .xxxxx code code, .xxxxx .xxxxx code {
@@ -139,7 +139,7 @@ body .to-extend, body .test {
   margin-left: 5px; }
   .pagination-bullet:first-child {
     margin-left: 0; }
-  .pagination-bullet.is-active {
+  .is-active.pagination-bullet {
     background-color: white; }
 
 .parent-nested-foo-include .in-nested-foo .child-nested-foo-include, .parent-nested-foo-include .in-nested-foo .beard + .mustache {

tests/outputs/extends_nesting.css
@@ -22,9 +22,9 @@
   .dropup + .btn + .caret, .extend .dropup + .the-button + .caret {
     content: "dropup + btn + caret"; }
 
-.dropup.btn, .extend .the-button.dropup {
+.dropup.btn, .extend .dropup.the-button {
   content: "dropupbtn"; }
-  .dropup.btn.caret, .extend .the-button.dropup.caret {
+  .dropup.btn.caret, .extend .dropup.caret.the-button {
     content: "dropupbtncaret"; }
 
 .nav-bar, header ul {

tests/outputs/selector_functions.css
@@ -60,7 +60,7 @@ a.disabled {
 .accordion__copy, .slider__copy, .accordion__image, .slider__image {
   content: '.accordion__copy, .slider__copy, .accordion__image, .slider__image'; }
 
-a.disabled, .link.disabled {
+a.disabled, .disabled.link {
   content: "a.disabled, .link.disabled"; }
 
 a.disabled {
@@ -87,7 +87,7 @@ ul li {
     content: ".sidebar";
     content: "p"; }
 
-.link.disabled {
+.disabled.link {
   content: ".link.disabled"; }
 
 a.disabled {

tests/outputs_numbered/extends.css
@@ -31,7 +31,7 @@ a:hover, .hoverlink, #demo .overview .fakelink:hover {
   text-decoration: underline; }
 
 /* line 40, inputs/extends.scss */
-div.hello.world.hmm, pre div.okay.span.world.hmm, pre #butt .umm div.sure.span.world.hmm, #butt .umm pre div.sure.span.world.hmm, code div.okay.span.world.hmm, code #butt .umm div.sure.span.world.hmm, #butt .umm code div.sure.span.world.hmm {
+div.hello.world.hmm, pre div.world.hmm.okay.span, pre #butt .umm div.world.hmm.span.sure, #butt .umm pre div.world.hmm.span.sure, code div.world.hmm.okay.span, code #butt .umm div.world.hmm.span.sure, #butt .umm code div.world.hmm.span.sure {
   color: blue; }
 
 /* line 44, inputs/extends.scss */
@@ -228,7 +228,7 @@ body .to-extend, body .test {
   margin-left: 0; }
 
 /* line 298, inputs/extends.scss */
-.pagination-bullet.is-active {
+.is-active.pagination-bullet {
   background-color: white; }
 
 /* line 303, inputs/extends.scss */

tests/outputs_numbered/extends_nesting.css
@@ -35,11 +35,11 @@
   content: "dropup + btn + caret"; }
 
 /* line 29, inputs/extends_nesting.scss */
-.dropup.btn, .extend .the-button.dropup {
+.dropup.btn, .extend .dropup.the-button {
   content: "dropupbtn"; }
 
 /* line 31, inputs/extends_nesting.scss */
-.dropup.btn.caret, .extend .the-button.dropup.caret {
+.dropup.btn.caret, .extend .dropup.caret.the-button {
   content: "dropupbtncaret"; }
 
 /* line 36, inputs/extends_nesting.scss */

commit 5df1e091a6f41dfda812ceefc60855716b04e00a

Fix #72 : 2bea5ef6 broke some case : pseudo selectors from the extendee have to been kept untouched as much as possible

src/Compiler.php
@@ -874,6 +874,11 @@ class Compiler
         $tag    = [];
         $out    = [];
         $wasTag = false;
+        $pseudo = [];
+
+        while (count($other) && strpos(end($other), ':')===0) {
+            array_unshift($pseudo, array_pop($other));
+        }
 
         foreach ([array_reverse($base), array_reverse($other)] as $single) {
             foreach ($single as $part) {
@@ -896,6 +901,9 @@ class Compiler
 
         if (count($tag)) {
             array_unshift($out, $tag[0]);
+        }
+        while (count($pseudo)) {
+            $out[] = array_shift($pseudo);
         }
 
         return $out;

tests/outputs/extending_compound_selector.css
@@ -1,6 +1,6 @@
 .foo:after, .bar:after, .bar:before, .baz.bar:before {
   color: red; }
-.foo:hover:after, .bar:after:hover, .bar:before:hover, .baz.bar:before:hover {
+.foo:hover:after, .bar:hover:after, .bar:before:hover, .baz.bar:before:hover {
   color: blue; }
 
 a, a:link, a:visited, a:active, a:hover {

tests/outputs_numbered/extending_compound_selector.css
@@ -4,7 +4,7 @@
   color: red; }
 
 /* line 5, inputs/extending_compound_selector.scss */
-.foo:hover:after, .bar:after:hover, .bar:before:hover, .baz.bar:before:hover {
+.foo:hover:after, .bar:hover:after, .bar:before:hover, .baz.bar:before:hover {
   color: blue; }
 
 /* line 10, inputs/extending_compound_selector.scss */