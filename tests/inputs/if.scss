scssphp/scssphp

commit f7e3e745df03b1271646e7e12614bdc800f6e851

Follow f9f86431 : Fix the enclosedList parsing in a more generic way

src/Parser.php
@@ -619,7 +619,9 @@ class Parser
 
             $this->seek($s);
 
-            if ($this->literal('@if', 3) && $this->valueList($cond) && $this->matchChar('{', false)) {
+            if ($this->literal('@if', 3)
+                && $this->functionCallArgumentsList($cond, false)
+                && $this->matchChar('{', false)) {
                 if ($this->cssOnly) {
                     $this->throwParseError("SCSS syntax not allowed in CSS file");
                 }
@@ -642,7 +644,7 @@ class Parser
             $this->seek($s);
 
             if ($this->literal('@debug', 6) &&
-                $this->valueList($value) &&
+                $this->functionCallArgumentsList($value, false) &&
                 $this->end()
             ) {
                 if ($this->cssOnly) {
@@ -657,7 +659,7 @@ class Parser
             $this->seek($s);
 
             if ($this->literal('@warn', 5) &&
-                $this->valueList($value) &&
+                $this->functionCallArgumentsList($value, false) &&
                 $this->end()
             ) {
                 if ($this->cssOnly) {
@@ -672,7 +674,7 @@ class Parser
             $this->seek($s);
 
             if ($this->literal('@error', 6) &&
-                $this->valueList($value) &&
+                $this->functionCallArgumentsList($value, false) &&
                 $this->end()
             ) {
                 if ($this->cssOnly) {
@@ -1717,6 +1719,38 @@ class Parser
     }
 
     /**
+     * Parse a function call, where externals () are part of the call
+     * and not of the value list
+     *
+     * @param $out
+     * @param bool $mandatoryParenthesis
+     * @return bool
+     */
+    protected function functionCallArgumentsList(&$out, $mandatoryParenthesis = true)
+    {
+        $s = $this->count;
+
+        if ($this->matchChar('(')
+            && $this->valueList($out)
+            && $this->matchChar(')') ) {
+
+            return true;
+        }
+
+        if (! $mandatoryParenthesis) {
+            $this->seek($s);
+
+            if ($this->valueList($out)) {
+                return true;
+            }
+        }
+
+        $this->seek($s);
+
+        return false;
+    }
+
+    /**
      * Parse space separated value list
      *
      * @param array $out
@@ -1870,7 +1904,7 @@ class Parser
         }
 
         if ($this->valueList($out) && $this->matchChar($closingParen) &&
-            \in_array($out[0], [Type::T_LIST, Type::T_KEYWORD, Type::T_NUMBER, Type::T_COLOR, Type::T_VARIABLE]) &&
+            ! ($closingParen === ")" && \in_array($out[0], [Type::T_EXPRESSION, Type::T_UNARY])) &&
             \in_array(Type::T_LIST, $allowedTypes)
         ) {
             if ($out[0] !== Type::T_LIST || ! empty($out['enclosing'])) {

commit f634760746bd1d6c465c62b50d4bbcf8b0478d55

Fix #165 : @if parsing has been broken by f7e3e745df03b1271646e7e12614bdc800f6e851 in some cases

src/Parser.php
@@ -634,8 +634,7 @@ class Parser
 
             if (
                 $this->literal('@if', 3) &&
-                $this->functionCallArgumentsList($cond, false) &&
-                $this->matchChar('{', false)
+                $this->functionCallArgumentsList($cond, false, '{', false)
             ) {
                 ! $this->cssOnly || $this->assertPlainCssValid(false, $s);
 
@@ -660,8 +659,7 @@ class Parser
 
             if (
                 $this->literal('@debug', 6) &&
-                $this->functionCallArgumentsList($value, false) &&
-                $this->end()
+                $this->functionCallArgumentsList($value, false)
             ) {
                 ! $this->cssOnly || $this->assertPlainCssValid(false, $s);
 
@@ -674,8 +672,7 @@ class Parser
 
             if (
                 $this->literal('@warn', 5) &&
-                $this->functionCallArgumentsList($value, false) &&
-                $this->end()
+                $this->functionCallArgumentsList($value, false)
             ) {
                 ! $this->cssOnly || $this->assertPlainCssValid(false, $s);
 
@@ -688,8 +685,7 @@ class Parser
 
             if (
                 $this->literal('@error', 6) &&
-                $this->functionCallArgumentsList($value, false) &&
-                $this->end()
+                $this->functionCallArgumentsList($value, false)
             ) {
                 ! $this->cssOnly || $this->assertPlainCssValid(false, $s);
 
@@ -1936,16 +1932,19 @@ class Parser
      *
      * @param $out
      * @param bool $mandatoryParenthesis
+     * @param null|string $expectingCharAfter
+     * @param null|bool $eatWhiteSpace
      * @return bool
      */
-    protected function functionCallArgumentsList(&$out, $mandatoryParenthesis = true)
+    protected function functionCallArgumentsList(&$out, $mandatoryParenthesis = true, $expectingCharAfter = null, $eatWhiteSpace = null)
     {
         $s = $this->count;
 
         if (
             $this->matchChar('(') &&
             $this->valueList($out) &&
-            $this->matchChar(')')
+            $this->matchChar(')') &&
+            ($expectingCharAfter ? $this->matchChar($expectingCharAfter, $eatWhiteSpace) : $this->end())
         ) {
             return true;
         }
@@ -1953,7 +1952,8 @@ class Parser
         if (! $mandatoryParenthesis) {
             $this->seek($s);
 
-            if ($this->valueList($out)) {
+            if ($this->valueList($out) &&
+                ($expectingCharAfter ? $this->matchChar($expectingCharAfter, $eatWhiteSpace) : $this->end())) {
                 return true;
             }
         }

tests/inputs/if.scss
@@ -78,3 +78,10 @@ $return: (1);
 @if $return == () {
     @warn 'empty list';
 }
+
+// @if with parenthesis but no global parenthesis
+$test: "1";
+$test2: "2";
+@if ($test == "1") and ($test2 == "2") {
+  .debug {color:red;}
+}

tests/outputs/if.css
@@ -19,3 +19,6 @@ div {
 
 del {
   thing: no; }
+
+.debug {
+  color: red; }

tests/outputs_numbered/if.css
@@ -24,3 +24,7 @@ div {
 /* line 67, inputs/if.scss */
 del {
   thing: no; }
+
+/* line 86, inputs/if.scss */
+.debug {
+  color: red; }