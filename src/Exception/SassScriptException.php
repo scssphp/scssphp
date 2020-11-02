ce5c933eb70bb1c579ad95834849fd0899e688cd
Christophe Coevoet <stof@notk.org>
2020-11-02 21:34:33 +0100

    Add a factory method for SassScriptException with an argument name

--- src/Exception/SassScriptException.php
+++ src/Exception/SassScriptException.php
@@ -11,4 +11,21 @@ namespace ScssPhp\ScssPhp\Exception;
  */
 class SassScriptException extends \Exception
 {
+    /**
+     * Creates a SassScriptException with support for an argument name.
+     *
+     * This helper ensures a consistent handling of argument names in the
+     * error message, without duplicating it.
+     *
+     * @param string      $message
+     * @param string|null $name    The argument name, without $
+     *
+     * @return SassScriptException
+     */
+    public static function forArgument($message, $name = null)
+    {
+        $varDisplay = !\is_null($name) ? "\${$name}: " : '';
+
+        return new self($varDisplay . $message);
+    }
 }
