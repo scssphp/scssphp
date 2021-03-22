3c105d7d4d0a43e5fa2cb2a444859c3d481ec408
Christophe Coevoet <stof@notk.org>
2021-03-22 09:39:58 +0100

    Add the base parser class

--- src/Util/Character.php
+++ src/Util/Character.php
@@ -18,6 +18,40 @@ namespace ScssPhp\ScssPhp\Util;
 final class Character
 {
     /**
+     * Returns whether $character is an ASCII whitespace character.
+     */
+    public static function isWhitespace(?string $character): bool
+    {
+        return $character === ' ' || $character === "\t" || $character === "\n" || $character === "\r" || $character === "\f";
+    }
+
+    /**
+     * Returns whether $character is a space or a tab character.
+     */
+    public static function isSpaceOrTab(?string $character): bool
+    {
+        return $character === ' ' || $character === "\t";
+    }
+
+    /**
+     * Returns whether $character is an ASCII newline character.
+     */
+    public static function isNewline(?string $character): bool
+    {
+        return $character === "\n" || $character === "\r" || $character === "\f";
+    }
+
+    /**
+     * Returns whether $character is a letter.
+     */
+    public static function isAlphabetic(string $character): bool
+    {
+        $charCode = \ord($character);
+
+        return ($charCode >= \ord('a') && $charCode <= \ord('z')) || ($charCode >= \ord('A') && $charCode <= \ord('Z'));
+    }
+
+    /**
      * Returns whether $character is a digit.
      */
     public static function isDigit(string $character): bool
@@ -25,6 +59,22 @@ final class Character
         $charCode = \ord($character);
 
         return $charCode >= \ord('0') && $charCode <= \ord('9');
+    }
+
+    /**
+     * Returns whether $character is legal as the start of a Sass identifier.
+     */
+    public static function isNameStart(string $character): bool
+    {
+        return $character === '_' || self::isAlphabetic($character) || \ord($character) >= 0x80;
+    }
+
+    /**
+     * Returns whether $character is legal in the body of a Sass identifier.
+     */
+    public static function isName(string $character): bool
+    {
+        return self::isNameStart($character) || self::isDigit($character) || $character === '-';
     }
 
     /**
@@ -47,5 +97,26 @@ final class Character
         }
 
         return false;
+    }
+
+    /**
+     * Assumes that $character is a left-hand brace-like character, and returns
+     * the right-hand version.
+     */
+    public static function opposite(string $character): string
+    {
+        switch ($character) {
+            case '(':
+                return ')';
+
+            case '{':
+                return '}';
+
+            case '[':
+                return ']';
+
+            default:
+                throw new \InvalidArgumentException(sprintf('Expected a brace character. Got "%s"', $character));
+        }
     }
 }
