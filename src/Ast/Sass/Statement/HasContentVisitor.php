53741d31893c57a29d335ff94eca56fdd4d76b6d
Christophe Coevoet <stof@notk.org>
2022-05-24 11:06:47 +0200

    Remove support for expressions in StatementSearchVisitor
    This support has been removed in dart-sass as it was not necessary.

--- src/Ast/Sass/Statement/HasContentVisitor.php
+++ src/Ast/Sass/Statement/HasContentVisitor.php
@@ -12,9 +12,6 @@
 
 namespace ScssPhp\ScssPhp\Ast\Sass\Statement;
 
-use ScssPhp\ScssPhp\Ast\Sass\ArgumentInvocation;
-use ScssPhp\ScssPhp\Ast\Sass\Interpolation;
-use ScssPhp\ScssPhp\Ast\Sass\SupportsCondition;
 use ScssPhp\ScssPhp\Visitor\StatementSearchVisitor;
 
 /**
@@ -30,20 +27,5 @@ final class HasContentVisitor extends StatementSearchVisitor
     public function visitContentRule(ContentRule $node): bool
     {
         return true;
-    }
-
-    protected function visitArgumentInvocation(ArgumentInvocation $invocation): ?bool
-    {
-        return null;
-    }
-
-    protected function visitSupportsCondition(SupportsCondition $condition): ?bool
-    {
-        return null;
-    }
-
-    protected function visitInterpolation(Interpolation $interpolation): ?bool
-    {
-        return null;
     }
 }
