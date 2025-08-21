3ff3801e4578b1873fb2ece514e7c634c94e072e
Christophe Coevoet <stof@notk.org>
2025-08-21 02:38:58 +0200

    Fix the detection of the best overload for error messages

--- src/SassCallable/BuiltInCallable.php
+++ src/SassCallable/BuiltInCallable.php
@@ -112,7 +112,7 @@ class BuiltInCallable implements SassCallable
         foreach ($overloads as $args => $callback) {
             $processedOverloads[] = [
                 ArgumentDeclaration::parse("@function $name($args) {", url: $url),
-                $callback
+                $callback,
             ];
         }
 
@@ -175,7 +175,7 @@ class BuiltInCallable implements SassCallable
             $mismatchDistance = \count($overload[0]->getArguments()) - $positional;
 
             if ($minMismatchDistance !== null) {
-                if (abs($mismatchDistance) > $minMismatchDistance) {
+                if (abs($mismatchDistance) > abs($minMismatchDistance)) {
                     continue;
                 }
 
